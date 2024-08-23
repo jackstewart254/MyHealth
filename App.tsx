3891e50f25eb0865c699048a5651c7efdd66a1c0
Jack Stewart <jackmcleanstewart49@gmail.com>
2024-08-23 12:41:45 +0400

    Adding uploading sessions check function

--- App.tsx
+++ App.tsx
@@ -5,11 +5,12 @@ import NetInfo from '@react-native-community/netinfo';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import 'react-native-url-polyfill/auto';
 import {WorkoutTrackerProvider} from './contexts/workoutTracker';
-import {fetchUser, setStoreExercise} from './localStorage/insert';
+import {clearKey, fetchUser, setStoreExercise} from './localStorage/insert';
 import {fetchActiveWorkout} from './localStorage/fetch';
 import Navigation from './src/navigation/navigation';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Login from './auth/login';
+import {fetchSessions, handleCheckSession} from './hooks/fetch';
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,12 @@ const App = () => {
   const [isConnected, setIsConnected] = useState(null);
 
   useEffect(() => {
+    const fetch = async () => {
+      await handleCheckSession();
+    };
+
+    fetch();
+
     const unsubscribe = NetInfo.addEventListener(state => {
       setConnectionType(state.type);
       setIsConnected(state.isConnected);
--- hooks/fetch.tsx
+++ hooks/fetch.tsx
@@ -1,8 +1,26 @@
 import supabase from '../supabase';
+import {fetchSessions} from '../localStorage/fetch';
+import {insertWorkout} from './insert';
 
 const fetchExercises = async () => {
   const {data: exercises} = await supabase.from('exercises').select('*');
   return exercises;
 };
 
-export {fetchExercises};
+const handleCheckSession = async () => {
+  const {data: sessions} = await supabase.from('sessions').select('id');
+  const res = await fetchSessions();
+  let notStored = [];
+
+  for (let i = 0; i < res.length; i++) {
+    let present = sessions.find(sess => sess.id === res[i].id);
+    if (present === undefined) {
+      notStored.push(res[i]);
+    }
+  }
+  for (let i = 0; i < notStored.length; i++) {
+    await insertWorkout(notStored[i]);
+  }
+};
+
+export {fetchExercises, handleCheckSession};
